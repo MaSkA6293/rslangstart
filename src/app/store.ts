6ac2c81d76af13dfab2454c8a9a9fe330f321600
Oleg Shkalikov <maska6293@yandex.ru>
2022-08-14 16:46:11 +0300

    feat: add persist to the project

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,28 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
 import appReducer from './app';
 import { wordsApi } from './wordsApi';
 import { userApi } from './userApi';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, appReducer);
 
 export const store = configureStore({
   reducer: {
     app: appReducer,
     [wordsApi.reducerPath]: wordsApi.reducer,
     [userApi.reducerPath]: wordsApi.reducer,
+    persistedReducer,
   },
   middleware: (getDefaultMiddlware) =>
     getDefaultMiddlware().concat(wordsApi.middleware),
 });
+
+export const persistor = persistStore(store);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
+import { store, persistor } from './app/store';
 import App from './App';
 import './index.scss';
+
+import { PersistGate } from 'redux-persist/integration/react';
 
 const container = document.getElementById('root');
 const root = createRoot(container as Element);
@@ -13,7 +15,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
